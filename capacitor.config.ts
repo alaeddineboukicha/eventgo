66c48d4ca13c778154beb090cbcccdc35cf0d043
alaeddineboukicha <alaboukhicha2@gmail.com>
2026-04-02 14:06:10 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eventgo',
+  webDir: 'www'
+};
+
+export default config;
